081e6c34096d37c2b14b10bd2971c942f3bac759
peter <wchegesalome@gmail.com>
2024-09-17 02:24:22 -0400

    commit 1

--- die.py
+++ die.py
@@ -3,7 +3,7 @@ from random import randint
 class Die:
     """A class representing a single die."""
 
-    def __init__(self, num_sides=6):
+    def __init__(self, num_sides=8):
         """Assume a six-sided die."""
         self.num_sides = num_sides
 
--- die_visual.py
+++ die_visual.py
@@ -8,7 +8,7 @@ die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(50_000):
+for roll_num in range(1000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -25,6 +25,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Result of rolling a D6 and a D10 5000 times',
+my_layout = Layout(title='Result of rolling two D8 Dice 1000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data':data, 'layout': my_layout}, filename='d6_d10.html')
+offline.plot({'data':data, 'layout': my_layout}, filename='d8.html')
