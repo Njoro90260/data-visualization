516027c494d486bd293baec5b657a3a4c0c05ea9
peter <wchegesalome@gmail.com>
2024-09-18 08:17:14 -0400

    Plot high temps.

--- data/sitka_highs.py
+++ data/sitka_highs.py
@@ -1,5 +1,5 @@
 import csv
-
+import matplotlib.pyplot as plt
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -14,5 +14,16 @@ with open(filename) as f:
         except ValueError:
             # Handle missing or non-numeric temperature values
             print(f"Missing or invalid data at row: {row}")
-print(highs)
 
+# plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Formart plot.
+plt.title("Daily high temperature , First week of January 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
