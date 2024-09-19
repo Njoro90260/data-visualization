2740401a7a8b0ccb65cf87ada65c59f777835d83
peter <wchegesalome@gmail.com>
2024-09-19 15:03:51 -0400

    compared high and low temps for the whole year.

--- data/sitka_highs_lows.py
+++ data/sitka_highs_lows.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename, encoding='ISO-8859-1') as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -9,14 +9,16 @@ with open(filename, encoding='ISO-8859-1') as f:
 # for index, column_header in enumerate(header_row):
 #     print(index, column_header)
 
-    # Get industry and values from this file.
-    dates, highs = [], []
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         try:
             current_date = datetime.strptime(row[2], '%Y-%m-%d')
             high = int(row[5].strip().replace('"', ''))
+            low = int(row[6])
             dates.append(current_date)
             highs.append(high)
+            lows.append(low)
         except ValueError:
             # Handle missing or non-numeric temperature values
             print(f"Missing or invalid data at row: {row}")
@@ -24,11 +26,12 @@ with open(filename, encoding='ISO-8859-1') as f:
 # plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Formart plot.
-plt.title("Daily temperatures of January 2018", fontsize=24)
+plt.title("Daily high and low temperatures -2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
