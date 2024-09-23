c37425b38dcd254bfa1a7798cbedfe0d3b2fea74
peter <wchegesalome@gmail.com>
2024-09-23 09:25:10 -0400

    Visualize rainfall.

--- data/sitka_highs_lows.py
+++ data/sitka_highs_lows.py
@@ -10,31 +10,26 @@ with open(filename, encoding='ISO-8859-1') as f:
 #     print(index, column_header)
 
     # Get dates, and high and low temperatures from this file.
-    dates, highs, lows = [], [], []
+    dates, rainfall = [], []
     for row in reader:
         try:
             current_date = datetime.strptime(row[2], '%Y-%m-%d')
-            high = int(row[5].strip().replace('"', ''))
-            low = int(row[6])
+            rcp = float(row[3].strip().replace('"', ''))
+            rainfall.append(rcp)
             dates.append(current_date)
-            highs.append(high)
-            lows.append(low)
         except ValueError:
             # Handle missing or non-numeric temperature values
             print(f"Missing or invalid data at row: {row}")
 
-# plot the high temperatures.
-plt.style.use('seaborn')
+# plot the Rainfall temperatures.
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red', alpha=0.5)
-ax.plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+ax.plot(dates, rainfall)
 
 # Formart plot.
-plt.title("Daily high and low temperatures -2018", fontsize=24)
+plt.title("Daily Rainfall amount -2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-plt.ylabel("Temperature (F)", fontsize=16)
+plt.ylabel("Rainfall (mm)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
