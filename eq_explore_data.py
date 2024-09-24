018d6836ae821de4a9ec27f3376651c571515dce
njoro90620 <wchegesalome@gmail.com>
2024-09-24 10:12:07 +0300

    used a dictionary to map earthquakes.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -15,14 +15,16 @@ with open(filename) as f:
     all_eq_dicts = all_eq_data['features']
     mags, lons, lats, hover_texts = [], [], [], []
     for eq_dict in all_eq_dicts:
-        mag = eq_dict['properties']['mag']
-        lon = eq_dict['geometry']['coordinates'][0]
-        lat = eq_dict['geometry']['coordinates'][1]
-        title =  eq_dict['properties']['title']
-        mags.append(mag)
-        lons.append(lon)
-        lats.append(lat)
-        hover_texts.append(title)
+        temp_dict = {
+            'mag': eq_dict['properties']['mag'],
+            'lon': eq_dict['geometry']['coordinates'][0],
+            'lat': eq_dict['geometry']['coordinates'][1],
+            'title': eq_dict['properties']['title']
+        }
+        mags.append(temp_dict['mag'])
+        lons.append(temp_dict['lon'])
+        lats.append(temp_dict['lat'])
+        hover_texts.append(temp_dict['title'])
 
     # Map the earthquakes.
     data = [{
