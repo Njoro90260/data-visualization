7b1480139bf08bc782466d9c07cb385918087693
njoro90620 <wchegesalome@gmail.com>
2024-09-24 09:15:49 +0300

    Add more information about earth quakes.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -13,22 +13,29 @@ with open(filename) as f:
     #     json.dump(all_eq_data, f, indent=4)
 
     all_eq_dicts = all_eq_data['features']
-    mags, lons, lats = [], [], []
+    mags, lons, lats, hover_texts = [], [], [], []
     for eq_dict in all_eq_dicts:
         mag = eq_dict['properties']['mag']
         lon = eq_dict['geometry']['coordinates'][0]
         lat = eq_dict['geometry']['coordinates'][1]
+        title =  eq_dict['properties']['title']
         mags.append(mag)
         lons.append(lon)
         lats.append(lat)
+        hover_texts.append(title)
 
     # Map the earthquakes.
     data = [{
         'type': 'scattergeo',
         'lon': lons,
         'lat': lats,
+        'text': hover_texts,
         'marker': {
             'size': [5*mag for mag in mags],
+            'color': mags,
+            'colorscale': 'Viridis',
+            'reversescale': True,
+            'colorbar': {'title': 'Magnitude'}, 
         }
     }]
     my_layout = Layout(title='Global Earthquakes')
